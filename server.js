15ae5ec4216bcbf755b39715e009674b39eee0ce
Tamas Piros <me@tamas.io>
2015-06-15 10:04:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.configure(function() {
 	    npid.create('/var/run/advanced-chat.pid', true);
 	} catch (err) {
 	    console.log(err);
-	    process.exit(1);
+	    //process.exit(1);
 	}
 
 });
